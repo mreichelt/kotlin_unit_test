d9d5e0c0cd140650c2a8a3af19f908c4b449c5dd
Marc Reichelt <mcreichelt@gmail.com>
2018-09-14 09:40:01 +0200

    Create sample project for flaky Kotlin unit tests

---
+++ app/src/test/java/com/example/kotlinunittests/JavaExampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.example.kotlinunittests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JavaExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
